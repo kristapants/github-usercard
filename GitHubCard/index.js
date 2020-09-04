51b639f7050e327c035d6b5bdf534715de89547e
kristapants <peachykeen@gmail.com>
2020-09-03 22:06:34 -0500

    refactored code and commented purposes of higher order, call backs and promises.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,36 +1,40 @@
 import axios from 'axios'
 
+//gets my information and list of friends, invokes creating my card and invokes creating friend cards with information from a callback
 axios.get('https://api.github.com/users/kristapants')
-  .then(apiData => {
-    console.log(apiData)
-    cardMaker(apiData.data)
-    axios.get(apiData.data.followers_url)
-      .then(friendList => {
-        addFriends(friendList.data)
+  .then(apiData => { //apiData now refers to the JSON stuff in the get ask. 
+    cardMaker(apiData.data) 
+    const {followers_url} = apiData.data; //destructoring of this object. Extracting followers_url from apiData.data so that entering followers_url is already known to be from api.data.data
+    axios.get(followers_url) //making a new http request to get the followers list.
+      .then(friendList => { //follower list is now known at friendList
+        addFriends(friendList.data) //invokes addfriends function that makes friend cards using the data in friendList 
       })
-      .catch(err => {
+      .catch(err => { //why bother
         console.log(err)
       })
   })
-  .catch(err => {
+  .catch(err => { //big boss bother
     console.log(err)
   })
 
-const cardBox = document.querySelector('.cards')
-
-const addFriends = (friendList) => {
-  friendList.forEach(singleFriend => {
-    axios.get(`https://api.github.com/users/${singleFriend.login}`)
-    .then(singleFriendData => {
-      cardMaker(singleFriendData.data)
-    })
-    .catch(err => {
-      console.log(err)
-    })
+//callback invoked above. collects information on each of my followers
+const addFriends = (friendList) => { //declaring a function called addFriends that takes an array of objects argument
+  friendList.forEach( ( {login} ) => {// for each on the array of objects but it's just acting on the login key value from each object in the array.
+    axios.get(`https://api.github.com/users/${login}`) //new http get using the usernames from the friendlist array. since login was all that was pulled from the object, it doesn't need to be further specified
+      .then(singleFriendData => { //the single friends userdata is now called singleFriendData
+        cardMaker(singleFriendData.data) //run the information stored in singleFriendData.data through cardmaker.
+      })
+      .catch(err => { //whyyyyy
+        console.log(err)
+      })
   })
 }
 
-const cardMaker = ( {avatar_url, name, login, location, html_url, followers, following, bio} ) => {
+//Defines the container for all cards to have children appended to it. 
+const cardBox = document.querySelector('.cards') //the div that all the user cards need to go into
+
+//structure, definitions, style and creation of each card. 
+const cardMaker = ( {avatar_url, name, login, location, html_url, followers, following, bio} ) => { //destructured object data. a single object entered as parameter but only these key value pairs are being used so they do not need to be further specified later. 
   const card = document.createElement('div')
   const profilePic = document.createElement('img')
   const cardInfo = document.createElement('div')
@@ -73,16 +77,4 @@ const cardMaker = ( {avatar_url, name, login, location, html_url, followers, fol
   profileFollowers.textContent = `Followers: ${followers}`
   profileFollowing.textContent = `Following: ${following}`
   profileBio.textContent = bio
-
-  return cardBox
 }
-
-
-/*
-  List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
-*/
