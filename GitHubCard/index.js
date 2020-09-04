cff8552bcdecb3c6743505972eaa3a00df7d42f5
kristapants <peachykeen@gmail.com>
2020-09-03 21:35:40 -0500

    Created cards for followers programatically for stretch goal.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,82 +1,34 @@
 import axios from 'axios'
-/*
-  STEP 1: using axios, send a GET request to the following URL
-    (replacing the placeholder with your Github name):
-    https://api.github.com/users/<your name>
-*/
+
 axios.get('https://api.github.com/users/kristapants')
-/*
-  STEP 2: Inspect and study the data coming back, this is YOUR
-    github info! You will need to understand the structure of this
-    data in order to use it to build your component function
-
-    Skip to STEP 3.
-*/
-
-/*
-  STEP 4: Pass the data received from Github into your function,
-    and append the returned markup to the DOM as a child of .cards
-*/
   .then(apiData => {
     console.log(apiData)
     cardMaker(apiData.data)
-    return addFollowers(followersArray)
+    axios.get(apiData.data.followers_url)
+      .then(friendList => {
+        addFriends(friendList.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
   })
   .catch(err => {
     console.log(err)
   })
 
-/*
-  STEP 5: Now that you have your own card getting added to the DOM, either
-    follow this link in your browser https://api.github.com/users/<Your github name>/followers,
-    manually find some other users' github handles, or use the list found at the
-    bottom of the page. Get at least 5 different Github usernames and add them as
-    Individual strings to the friendsArray below.
-
-    Using that array, iterate over it, requesting data for each user, creating a new card for each
-    user, and adding that card to the DOM.
-*/
-const followersArray = ["deanbot", "LeTanque", "tetondan", "dustinmyers", "justsml"];
-
-const addFollowers = (friends) => {
-  friends.forEach(element => {
-    axios.get(`https://api.github.com/users/${element}`)
-      .then(friendData => {
-        console.log(friendData)
-        cardMaker(friendData.data)
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  });
-}
-
-
-
-
-
-
-
-/*
-  STEP 3: Create a function that accepts a single object as its only argument.
-    Using DOM methods and properties, create and return the following markup:
-
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
-      </div>
-    </div>
-*/
 const cardBox = document.querySelector('.cards')
+
+const addFriends = (friendList) => {
+  friendList.forEach(singleFriend => {
+    axios.get(`https://api.github.com/users/${singleFriend.login}`)
+    .then(singleFriendData => {
+      cardMaker(singleFriendData.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  })
+}
 
 const cardMaker = ( {avatar_url, name, login, location, html_url, followers, following, bio} ) => {
   const card = document.createElement('div')
